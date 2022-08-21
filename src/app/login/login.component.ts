8dfa5eae5321a1713187c6993002c18d799f9ec7
TecaFondo <carlos.kohler.r@mail.pucv.cl>
2022-08-21 19:43:33 -0400

    Se agrega validacion de datos (en pagina incorrecta, se necesita cambiar de lugar)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Platform} from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit() {}
+  ios: boolean;
+      android: boolean;
+      
+      constructor(public platform: Platform) {
+          this.ios = platform.is('ios');
+          this.android = platform.is('android');
+}
 
 }
