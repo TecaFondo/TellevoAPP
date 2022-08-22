b2d3d5e7e436c057c10ebb566d8e42ae5d4c845f
TecaFondo <carlos.kohler.r@mail.pucv.cl>
2022-08-22 13:34:17 -0400

    Se agregan validadores a nombre de ususario y contraseña de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Validators, FormControl, FormBuilder,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  ios: boolean; //esto es para detectar cuando un dispositivo es iOS
+  android: boolean; //same with android
 
-  constructor() { }
+  usuario={ //se declara un usuario para validar
+    nombre:'',
+    password:''
+  };
 
-  ngOnInit() {
-  }
+  constructor(public platform: Platform) { //valida tipo dispositivo (android o ios)
+    this.ios = platform.is('ios');
+    this.android = platform.is('android');
 
+}
+
+onSubmitTemplate(){ //Muestra datos de usuario (si son correctos <cumplen validacion>)
+  console.log('Form Submited');
+  console.log(this.usuario);
+}
 }
